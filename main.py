ac05175dc33b543935b34302ce47333396fe5855
adv27 <anhvd27@gmail.com>
2019-08-28 08:55:21 +0700

    migrate to selenium

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from time import sleep
 
 from bs4 import BeautifulSoup
-from requests import Session
+from selenium.webdriver import PhantomJS
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
-ENPOINT = 'http://www.uceprotect.net/en/rblcheck.php'
+ENDPOINT = 'http://www.uceprotect.net/en/rblcheck.php'
 WAIT_TIME = 60  # seconds
 
-session = Session()
-session.headers.update(
-    {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'})
+
+def get_phantomjs_path():
+    import platform
+
+    system = platform.system()
+    if system == 'Windows':
+        return './drivers/windows/phantomjs.exe'
+    if system == 'Linux':
+        return './drivers/linux/phantomjs'
+
+    return 'phantomjs'
 
 
 def get_ip_list(f_name='data.txt'):
@@ -24,25 +36,34 @@ def save_result(ip, status, listing_risk):
         file.write('{} | {} | {}\n'.format(ip, status, listing_risk))
 
 
-def get_subchannel():
-    r = session.get(ENPOINT)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    subchannel_input = soup.find('input', {'name': 'subchannel'})
-    subchannel = subchannel_input.get('value')
-    return subchannel
-
-
-def crawler(ip_address):
+def crawler(ip_address, webdriver):
     if not ip_address:
         return
-    subchannel = get_subchannel()
-    data = {
-        'whattocheck': 'IP',
-        'ipr': ip_address,
-        'subchannel': subchannel,
-    }
-    r = session.post(ENPOINT, data=data)
-    soup = BeautifulSoup(r.content, 'html.parser')
+
+    webdriver.get(ENDPOINT)
+
+    ip_address_field = webdriver.find_element_by_name('ipr')
+    ip_address_field.click()
+    ip_address_field.send_keys(ip_address)
+    submit_btn = webdriver.find_element_by_name('testform')
+    submit_btn.submit()
+
+    # wait until form sent
+    try:
+        element = WebDriverWait(webdriver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'db')))
+    except TimeoutException:
+        webdriver.save_screenshot('img/{}_error.png'.format(ip_address))
+    finally:
+        pass
+
+    filename = 'img/{}_result.png'.format(ip_address)
+    sss = webdriver.save_screenshot(filename)
+    if sss:
+        print('Success save result image to: {}'.format(filename))
+    else:
+        print('Failed to save result image!')
+    soup = BeautifulSoup(webdriver.page_source, 'html.parser')
     table = soup.find('table', {'class': 'db'})
     row = table.find_all('tr')[1]
     cols = row.find_all('td')
@@ -54,10 +75,11 @@ def crawler(ip_address):
 
 def main():
     ip_list = get_ip_list()
+    webdriver = PhantomJS(executable_path=get_phantomjs_path())
     for ip_address in ip_list:
         print('='*30)
         print('Checking: {}'.format(ip_address))
-        ip, status, listing_risk = crawler(ip_address)
+        ip, status, listing_risk = crawler(ip_address, webdriver)
         print('{} | {} | {}'.format(ip, status, listing_risk))
         print('='*30)
         save_result(ip, status, listing_risk)
